c6356e463b0dd5178a6462964c415610cbb5a078
anujgupta177 <56711453+anujgupta177@users.noreply.github.com>
2022-08-09 22:07:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import json
+import re
+import random
+
+f = open("readme.json")
+
+dataSet = json.load(f)
+
+dataSetNew = {}
+
+for key in dataSet:
+
+	matchGroup = re.match(r'rx_queue_(\d+)_packets',key)
+
+	if(matchGroup):
+
+	    random_num = random.randint(0,500)
+
+	    dataSet[key] = dataSet[key] + random_num
+
+
+print(dataSet)
+
+print(dataSetNew)
+
+f.close()
